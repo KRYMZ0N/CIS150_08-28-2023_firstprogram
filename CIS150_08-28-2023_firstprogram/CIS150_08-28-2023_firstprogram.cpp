d28d12d6635806a26374efb6dea857a3680d5799
jgrimes34528 <jgrimes34528@DT-015503>
2023-08-28 10:49:55 -0400

    Add project files.

---
+++ CIS150_08-28-2023_firstprogram/CIS150_08-28-2023_firstprogram.cpp
@@ -0,0 +1,15 @@
+//==========================================
+//Author: Jack Grimes
+//Date: 08-28-23
+//Description: First program, Hello World!
+//==========================================
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    //All code for this class until I state other wise will go between these brackets
+    cout << "Hello World!\n";
+    cout << "Jack Grimes" << endl;
+}
